02e47df90d2c25207e59ad220ef3460d9008d91a
Auutumn <117687231+Auutumn@users.noreply.github.com>
2024-11-15 11:03:05 -0300

    Add files via upload

---
+++ carrusel.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const container = document.querySelector(".carrusel-container");
+    const slides = document.querySelectorAll(".carrusel-slide");
+    let currentIndex = 0;
+
+    function showNextSlide() {
+        currentIndex = (currentIndex + 1) % slides.length; // Calcula el próximo índice
+        container.style.transform = `translateX(-${currentIndex * 100}%)`; // Mueve el contenedor para mostrar el siguiente slide
+    }
+
+    // Cambia de slide cada 3 segundos
+    setInterval(showNextSlide, 3000);
+});
